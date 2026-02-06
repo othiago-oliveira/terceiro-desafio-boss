9df6918495544b690204dc431f19d0901c0f4de9
othiago-oliveira <thiagodomp3@gmail.com>
2026-02-06 21:07:31 +0000

    Update terceiro-desafio-boss.js

--- terceiro-desafio-boss.js
+++ terceiro-desafio-boss.js
@@ -3,29 +3,22 @@ class heroi{
     this.nome = nome
     this.idade = idade
     this.tipo = tipo 
-	
     }
-    
     atacar(){
     	let ataque = ""
-        
     	if(this.tipo === "mago"){
         	ataque = "magia"
         }
         else if(this.tipo === "guerreiro"){
-        	ataque = "espada"
-        	
+        	ataque = "espada"	
         }
         else if(this.tipo === "monge"){
-        	ataque = "artes marciais"
-        	
+        	ataque = "artes marciais"	
         }
         else if (this.tipo === "ninja"){
         	ataque = "shuriken"
-        
         }
 		console.log (`O ${this.tipo} atacou usando ${ataque}`)
-
 	}
 }
 
